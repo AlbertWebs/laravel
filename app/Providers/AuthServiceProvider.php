60f20ee1b95cedc80a4d3714735856ce7bc11fd5
Zach Leigh <zachleigh@fastmail.jp>
2016-11-23 09:29:36 +0900

    Add gate policy.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('manage-portfolio', function ($user) {
+            return $user->id == 1;
+        });
     }
 }

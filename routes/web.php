17fb6d6d76af47f309acd3dadcf95c5903b86b01
Zach Leigh <zachleigh@fastmail.jp>
2016-11-23 09:16:22 +0900

    Add support routes.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Database\Eloquent\Factory as EloquentFactory;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    app()->make(EloquentFactory::class)->load(__DIR__.'/../../../../src/database/factories/');
+
+    Artisan::call('migrate:refresh');
+
+    Artisan::call('db:seed', [
+        '--class' => 'Larafolio\database\seeds\DatabaseSeeder'
+    ]);
+
+    $user = App\User::first();
+
+    Auth::login($user);
+
+    return redirect('/manager');
 });
